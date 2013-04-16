3332117e57f36271cb6fc2d46508264631edb251
Daniel <Daniel@10.99.18.149>
2013-04-16 13:59:26 +0200

    modifications

--- src/Domain/Order.java
+++ src/Domain/Order.java
@@ -16,99 +16,122 @@ public class Order {
     private int customerID, orderID, unitSize;
     private String adress;
     private Date startDate, endDate;
-    private boolean deposit;
-    private double finalPrice, discount;
+    private boolean canceled;
+    private double fullPrice, discount, aditionalCost;
+    private byte paymantState;
 
-    public Order(int customerID, int orderID, int unitSize, String eventAddress, Date startDate, Date endDate, boolean deposit, double finalPrice, double discount) {
+    public Order( int customerID, int orderID, int unitSize, String adress, Date startDate, Date endDate, boolean canceled, double fullPrice, double discount, double aditionalCost, byte paymantState) {
+        this.orderDetails = orderDetails;
         this.customerID = customerID;
         this.orderID = orderID;
         this.unitSize = unitSize;
-        this.adress = eventAddress;
+        this.adress = adress;
         this.startDate = startDate;
         this.endDate = endDate;
-        this.deposit = deposit;
-        orderDetails = new ArrayList();
-        this.finalPrice = finalPrice;
+        this.canceled = canceled;
+        this.fullPrice = fullPrice;
         this.discount = discount;
-    }
-    
-    public void setDiscount(double discount){
-        this.discount = discount;
-    }
-    
-    public double getDiscount(){
-        return discount;
-    }
-    
-    public void setFinalPrice(double finalPrice){
-        this.finalPrice = finalPrice;
-    }
-    
-    public double getFinalPrice(){
-        return finalPrice;
+        this.aditionalCost = aditionalCost;
+        this.paymantState = paymantState;
     }
 
-    public void insertOrderDetail(OrderDetail orderDetail){
-        orderDetails.add(orderDetail);
+    public ArrayList<OrderDetail> getOrderDetails() {
+        return orderDetails;
     }
-    
+
     public int getCustomerID() {
         return customerID;
-    }
-
-    public void setCustomerID(int customerID) {
-        this.customerID = customerID;
     }
 
     public int getOrderID() {
         return orderID;
     }
 
-    public void setOrderID(int orderID) {
-        this.orderID = orderID;
-    }
-
     public int getUnitSize() {
         return unitSize;
-    }
-
-    public void setUnitSize(int unitSize) {
-        this.unitSize = unitSize;
     }
 
     public String getAdress() {
         return adress;
     }
 
-    public void setAdress(String adress) {
-        this.adress = adress;
-    }
-
     public Date getStartDate() {
         return startDate;
-    }
-
-    public void setStartDate(Date startDate) {
-        this.startDate = startDate;
     }
 
     public Date getEndDate() {
         return endDate;
     }
 
+    public boolean isCanceled() {
+        return canceled;
+    }
+
+    public double getFullPrice() {
+        return fullPrice;
+    }
+
+    public double getDiscount() {
+        return discount;
+    }
+
+    public double getAditionalCost() {
+        return aditionalCost;
+    }
+
+    public byte getPaymantState() {
+        return paymantState;
+    }
+
+    public void setOrderDetails(ArrayList<OrderDetail> orderDetails) {
+        this.orderDetails = orderDetails;
+    }
+
+    public void setCustomerID(int customerID) {
+        this.customerID = customerID;
+    }
+
+    public void setOrderID(int orderID) {
+        this.orderID = orderID;
+    }
+
+    public void setUnitSize(int unitSize) {
+        this.unitSize = unitSize;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
     public void setEndDate(Date endDate) {
         this.endDate = endDate;
     }
 
-    public boolean isDeposit() {
-        return deposit;
+    public void setCanceled(boolean canceled) {
+        this.canceled = canceled;
     }
 
-    public void setDeposit(boolean deposit) {
-        this.deposit = deposit;
+    public void setFullPrice(double fullPrice) {
+        this.fullPrice = fullPrice;
     }
-    
-    public String toString(){
-        return orderID + "-" + customerID;
+
+    public void setDiscount(double discount) {
+        this.discount = discount;
+    }
+
+    public void setAditionalCost(double aditionalCost) {
+        this.aditionalCost = aditionalCost;
+    }
+
+    public void setPaymantState(byte paymantState) {
+        this.paymantState = paymantState;
+    }
+
+    public void insertOrderDetail(OrderDetail orderDetail){
+        orderDetails.add(orderDetail);
     }
 }
--- src/Presentation/Main.java
+++ src/Presentation/Main.java
@@ -20,25 +20,33 @@ public class Main {
         Controller control = Controller.getInstance();
         ArrayList<Resource> availableResources = new ArrayList();
         ArrayList<Truck> trucks = new ArrayList();
+        ArrayList<Order> orders = new ArrayList();
+        
+        control.getConnection();
         
         DateFormat df = new SimpleDateFormat("dd-MM-yyyy");
             java.util.Date startD = df.parse("31-12-1995");
             java.util.Date endD = df.parse("02-10-2001");
        
-        availableResources = control.getAvailableResources(startD ,endD );
-        System.out.println(availableResources.toString());
-//  "31-12-1995", "02-10-2001"      
-        for(int i=0; i<availableResources.size(); i++){
-            System.out.println(availableResources.get(i).getResourceName()+"  "+availableResources.get(i).getQuantity()
-                );
-        }
+//        availableResources = control.getAvailableResources(startD ,endD );
+//        System.out.println(availableResources.toString());
+////  "31-12-1995", "02-10-2001"      
+//        for(int i=0; i<availableResources.size(); i++){
+//            System.out.println(availableResources.get(i).getResourceName()+"  "+availableResources.get(i).getQuantity()
+//                );
+//        }
        
-        trucks = control.getTruckDeliveryForDate(startD,'0');
-         for(int i=0; i<trucks.size(); i++){
-            System.out.println(trucks.get(i).getTruckID()+"  "+trucks.get(i).getTruckRun()+" "+ trucks.get(i).getFilledSpace()+ " "+ trucks.get(i).getSize()
+//        trucks = control.getTruckDeliveryForDate(startD,'0');
+//         for(int i=0; i<trucks.size(); i++){
+//            System.out.println(trucks.get(i).getTruckID()+"  "+trucks.get(i).getTruckRun()+" "+ trucks.get(i).getFilledSpace()+ " "+ trucks.get(i).getSize()
+//                );
+//        }
+            
+          orders = control.getOrders();
+          for(int i=0; i<orders.size(); i++){
+          System.out.println(orders.get(i).toString()
                 );
-        }
-//           control.getOrders();
+          }
 //            
          
         
